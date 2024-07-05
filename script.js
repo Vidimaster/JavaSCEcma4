8ae445725e4afff2c1cd1eede71369b4c04f6bd0
Vidimaster <vidimaster@yandex.com>
2024-07-05 12:32:47 +0700

    tasks done

--- script.js
+++ script.js
@@ -1,99 +1,99 @@
-// Домашнее задание:
-// Задание 1: "Управление персоналом компании"
+// Домашнее задание
 
-// Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
-// Свойство name (имя) - строка, имя сотрудника.
-// Метод displayInfo() - выводит информацию о сотруднике (имя) в консоль.
+// Задание 1. Получение данных о пользователе.
 
-// Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
-// Свойство department (отдел) - строка, отдел, в котором работает менеджер.
-// Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+// Реализуйте функцию getUserData, которая принимает идентификатор пользователя (ID) в качестве аргумента и использует fetch для получения данных о пользователе с заданным ID с удаленного сервера. Функция должна возвращать промис, который разрешается с данными о пользователе в виде объекта. Если пользователь с указанным ID не найден, промис должен быть отклонен с соответствующим сообщением об ошибке.
 
-// // Пример использования классов
-// const employee = new Employee("John Smith");
-// employee.displayInfo(); // "Name: John Smith"
+// Подсказка, с последовательностью действий:
+// getUserData использует fetch для получения данных о пользователе с удаленного сервера. Если запрос успешен (с кодом 200), функция извлекает данные из ответа с помощью response.json() и возвращает объект с данными о пользователе. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
 
-// const manager = new Manager("Jane Doe", "Sales");
-// manager.displayInfo(); // "Name: John Doe, Department: Sales"
+// Работа должна быть выполнена с API: https://reqres.in/
 
-class Employee {
-    constructor(name) {
-        this.name = name;
+async function getUserData(id) {
+    const response = await fetch('https://reqres.in/api/users/' + id);
+    if (response.status != 200) {
+        return json.then(Promise.reject.bind(Promise));
     }
-    displayInfo() {
-        console.log(`${this.constructor.name} name: ${this.name}`);
+    else {
+        return response.json();
     }
 }
 
-const employee = new Employee("John Smith");
+const id = 10;
 
-class Manager extends Employee {
-    constructor(name, department) {
-        super(name);
-        this.department = department;
+getUserData(id).then((data) => {
+    console.log(data);
+}).catch((error) => {
+    alert(`${error.message}`);
+})
+
+// Задание 2. Отправка данных на сервер.
+
+// Реализуйте функцию saveUserData, которая принимает объект с данными о пользователе в качестве аргумента и использует fetch для отправки этих данных на удаленный сервер для сохранения. Функция должна возвращать промис, который разрешается, если данные успешно отправлены, или отклоняется в случае ошибки.
+
+// *Подсказка *
+
+// // Пример использования функции
+// const user = {
+//   "name": "John Doe",
+//   "job": "unknown"
+// };
+
+// saveUserData(user)
+//   .then(() => {
+//     console.log('User data saved successfully');
+//   })
+//   .catch(error => {
+//     console.log(error.message);
+//   });
+
+// saveUserData использует fetch для отправки данных о пользователе на удаленный сервер для сохранения. Она отправляет POST-запрос на URL-адрес /api/users с указанием типа содержимого application/json и сериализует объект с данными о пользователе в JSON-строку с помощью JSON.stringify(). Если запрос успешен (с кодом 201), функция разрешает промис. Если запрос неуспешен, функция отклоняет промис с сообщением об ошибке.
+
+// Работа должна быть выполнена с API: https://reqres.in/
+
+async function saveUserData(data) {
+    const response = await fetch('https://reqres.in/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.status != 201) {
+        return json.then(Promise.reject.bind(Promise));
     }
-    displayInfo() {
-        console.log(`${this.constructor.name} name: ${this.name}, department: ${this.department}`);
-    }
-}
-
-const manager = new Manager("Jane Doe", "Sales");
-
-manager.displayInfo();
-employee.displayInfo();
-
-// Задание 2: "Управление списком заказов"
-
-// Реализуйте класс Product (товар), который имеет следующие свойства и методы:
-// Свойство name - название товара.
-// Свойство price - цена товара.
-// Свойство quantity - количество товара.
-
-// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
-// Свойство id (номер заказа) - число, уникальный номер заказа.
-// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
-// Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
-// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
-
-// // Пример использования:
-// const order = new Order(12345);
-
-// const product1 = new Product("Phone", 500, 2);
-// order.addProduct(product1);
-
-// const product2 = new Product("Headphones", 100, 1);
-// order.addProduct(product2);
-
-// console.log(order.getTotalPrice()); // Вывод: 1100
-
-class Product {
-    constructor(name, price, quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-}
-
-class Order {
-    products = new Array();
-    constructor(id) {
-        this.id = id;
-    }
-    addProduct(obj) {
-        this.products.push(obj);
-    }
-    getTotalPrice() {
-        let res = 0;
-        this.products.forEach(element => {
-            res = res + element.price * element.quantity;
-        });
-        return res;
+    else {
+        return response.json();
     }
 }
 
-const order = new Order(12345);
-const product1 = new Product("Phone", 500, 2);
-order.addProduct(product1);
-const product2 = new Product("Headphones", 100, 1);
-order.addProduct(product2);
-console.log(order.getTotalPrice()); 
+const user = {
+    "name": "John Doe",
+    "job": "unknown"
+};
+
+saveUserData(user).then((data) => {
+    console.log(data);
+    console.log("User data saved");
+}).catch((error) => {
+    alert(`${error.message}`);
+})
+
+// Задание 3. Изменение стиля элемента через заданное время (выполняем, если знакомы с DOM).
+
+// Напишите функцию changeStyleDelayed, которая принимает id элемента и время задержки (в миллисекундах) в качестве аргументов. Функция должна изменить стиль (любой, например - цвет текста) элемента через указанное время.
+
+// // Пример использования функции
+// changeStyleDelayed('myElement', 2000); // Через 2 секунды изменяет стиль элемента с id 'myElement'"
+
+function changeStyleDelayed(elem, delay) {
+    setTimeout(() => {
+        document.querySelector("#" + elem).style.background = 'red';
+    }, delay);
+}
+
+changeStyleDelayed("p3", 2000);
+changeStyleDelayed("p4", 4000);
+changeStyleDelayed("p1", 6000);
+
